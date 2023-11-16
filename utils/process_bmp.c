0bc23dd963f4d9413303392145d2c8aa133900f5
cojocarucristian05 <cojocaru.cristian20@gmail.com>
2023-11-16 22:50:04 +0200

    Add color table to bmp format, add function for changing pixels

--- utils/process_bmp.c
+++ utils/process_bmp.c
@@ -7,65 +7,90 @@
 #include "writer.h"
 #include "process_bmp.h"
 
+#define RED_PROPORTION 0.299
+#define GREEN_PROPORTION 0.587
+#define BLUE_PROPORTION 0.114
+
+// functie care modifica valoarea pixelilor conform proportilor
+void convertToGrayScale(BmpFormat *bmpFormat) 
+{
+    int size = bmpFormat->infoHeader.width * bmpFormat->infoHeader.height;
+    for (int i = 0; i < size; i++) {
+        bmpFormat->colorTable[i].red = (char)(bmpFormat->colorTable[i].red * RED_PROPORTION);
+        bmpFormat->colorTable[i].green = (char)(bmpFormat->colorTable[i].green * GREEN_PROPORTION);
+        bmpFormat->colorTable[i].blue = (char)(bmpFormat->colorTable[i].blue * BLUE_PROPORTION);
+    }
+}
+
+// functie care proceseaza imaginea (citeste date, proceseaza pixeli, scrie date)
 void processImage(char *file_name, struct stat image_stat)
 {   
     char path[PATH_MAX];
     int imageFileDescriptor = 0;
     BmpFormat bmpFormat;
-    
+    int colorTableSize = 0;
+
     sprintf(path, "./dir/%s", file_name);
     
-    imageFileDescriptor = open(path, O_RDONLY);
+    // deschidere fisier
+    imageFileDescriptor = open(path, O_RDWR);
     if (imageFileDescriptor < 0)
     {
         perror("Eroare deschidere fisier intrare!");
         exit(EXIT_FAILURE);
     }
 
-    if (read(imageFileDescriptor, bmpFormat.header.signature, 2) != 2)
+    // citire header
+    if (read(imageFileDescriptor, &bmpFormat.header, sizeof(Header)) != sizeof(Header))
     {
-        perror("Eroare citire semnatura header!");
+        perror("Eroare citire header!");
         exit(EXIT_FAILURE);
     }
 
-    if (read(imageFileDescriptor, &bmpFormat.header.fileSize, 4) != 4)
+    // citire info header
+    if (read(imageFileDescriptor, &bmpFormat.infoHeader, sizeof(InfoHeader)) != sizeof(InfoHeader))
     {
-        perror("Eroare citire fileSize header!");
+        perror("Eroare citire info header!");
         exit(EXIT_FAILURE);
     }
 
-    if (read(imageFileDescriptor, &bmpFormat.header.reserved, 4) != 4)
+    // alocare memorie tablou pixeli
+    colorTableSize = bmpFormat.infoHeader.width * bmpFormat.infoHeader.height;
+    bmpFormat.colorTable = (ColorTable *)malloc(colorTableSize * sizeof(ColorTable));
+    if (bmpFormat.colorTable == NULL) 
     {
-        perror("Eroare citire reserved header!");
+        perror("Eroare alocare memorie!");
         exit(EXIT_FAILURE);
     }
 
-    if (read(imageFileDescriptor, &bmpFormat.header.dataOffset, 4) != 4)
+    // citire color table
+    if (read(imageFileDescriptor, bmpFormat.colorTable, sizeof(ColorTable) * colorTableSize) != sizeof(ColorTable) * colorTableSize)
     {
-        perror("Eroare citire dataOffset header!");
+        perror("Eroare citire color table!");
         exit(EXIT_FAILURE);
     }
 
-    if (read(imageFileDescriptor, &bmpFormat.infoHeader.size, 4) != 4)
+    // modificare pixeli
+    convertToGrayScale(&bmpFormat);
+
+    // mutam cursorul inapoi la pozitia de inceput a tabloului
+    if (lseek(imageFileDescriptor, bmpFormat.header.dataOffset, SEEK_SET) < 0)
     {
-        perror("Eroare citire size of infoheader!");
+        perror("Eroare setare cursor!");
         exit(EXIT_FAILURE);
     }
 
-    if (read(imageFileDescriptor, &bmpFormat.infoHeader.width, 4) != 4)
+    // scriere date, in fisierul bmp suprascriem valoarea pixelilor originali cu cei modificati
+    if (write(imageFileDescriptor, bmpFormat.colorTable, sizeof(ColorTable) * colorTableSize) != sizeof(ColorTable) * colorTableSize)
     {
-        perror("Eroare citire size of infoheader!");
+        perror("Eroare scriere pixeli modificati!");
         exit(EXIT_FAILURE);
     }
 
-    if (read(imageFileDescriptor, &bmpFormat.infoHeader.height, 4) != 4)
-    {
-        perror("Eroare citire size of infoheader!");
-        exit(EXIT_FAILURE);
-    }
-
+    // scriere statistici imagine
     writeImageStatistics(file_name, bmpFormat, image_stat);    
 
+    // inchidere fisier
     if (close(imageFileDescriptor) < 0)
     {
         perror("Eroare inchidere fisier intrare!");
